8cdb0d358a2a583ca3ae6a341e1029e0dc69ef06
Louis Anthony Agong <la@buri.io>
2022-02-16 14:33:06 +0800

    bot code scaffolding

---
+++ README.md
@@ -0,0 +1,11 @@
+# Leave Bot
+
+## Instructions
+
+1. Clone the repository
+1. Go to project folder
+1. Create a branch
+1. Run `npm install`
+1. Create `.env` in project root folder
+1. Add `TELEGRAM_API_KEY=<INSERT_API_KEY_HERE>` in the `.env` file
+1. Code and deploy using `npm start`
---
+++ services/bot.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
+
+bot.start((ctx) => ctx.reply("Welcome"));
+
+bot.help((ctx) => ctx.reply("Help is on the way"));
+bot.on("sticker", (ctx) => ctx.reply("👍"));
+
+bot.hears("test", (ctx) => {
+  console.log(ctx.update);
+});
+
+bot.launch();
