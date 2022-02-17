e224a210e8b250554499fd0f10778e183ba3e724
Ameer Fernandez <ameerfernandez@Zoraidas-Air.home.arpa>
2022-02-17 13:12:21 +0800

    date track, leave logging using array, no user id logging yet

--- services/bot.js
+++ services/bot.js
@@ -4,6 +4,12 @@ const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
 
 bot.start((ctx) => ctx.reply("Welcome"));
 
+let leaveLog = [];
+
+const d = new Date();
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const dateNow = `${month[d.getMonth()]} ${d.getDate()}`
+
 bot.help((ctx) => ctx.reply("Help is on the way"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 
@@ -11,4 +17,17 @@ bot.hears("test", (ctx) => {
   console.log(ctx.update);
 });
 
+bot.hears("hello", (ctx) => {
+  ctx.reply("world");
+});
+
+bot.hears("date", (ctx) => {
+  ctx.reply(dateNow)
+});
+
+bot.hears("/leave", (ctx) =>{
+  ctx.reply(dateNow);
+  leaveLog.push('userid')
+  console.log(leaveLog);
+})
 bot.launch();
